14c5666b6aa54c5748b6cde5a44b88f5f84d3ade
Isabelle Nascimento <noisabelle06@gmail>
2023-10-02 12:11:35 -0300

    adcionando js

--- controller.js
+++ controller.js
@@ -11,3 +11,41 @@ const limparFormulario = (endereco) =>{
 
 //Verifica se CEP é válido
 const eNumero = (numero) => /^[0-9]+$/.test(numero);
+// verifica se o cep é valido
+
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+//preenche campos do formulario
+const preencherFormulario = (endereco) =>{
+    document.getElementById('rua').value = endereco.logradouro;
+    document.getElementById('estado').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('bairro').value = endereco.uf;
+}
+/*
+função para consumo de API ultilizando a função do tipo assincrona
+*/
+const pesquisarCep = async() =>{
+    limparFormulario();
+    const url = `http://viacep.com.br/ws/${cep.value}/json/`;
+}
+
+
+if(cepValido(cep.value)){
+    const dados = await fetch(url);
+    const addres = await dados.json();
+
+    if (addres.hasOwnProperty('erro')){
+        alert('CEP não encontrado');
+
+    }else{
+        preencherFormulario(addres);
+    }
+ 
+}else{
+    alert ('CEP incorreto');
+
+}
+
+//adciona um evento DOM, no imput 
+document.getElementById('cep').addEventListener('focusout', pesquisarCep);
